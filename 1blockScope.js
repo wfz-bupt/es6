7773b9d279a4d7002241565ffe371e273783fd54
wangfengzi <1558581187@qq.com>
2017-07-18 06:59:59 +0800

    let const

--- 1blockScope.js
+++ 1blockScope.js
@@ -6,7 +6,19 @@
 用let声明的变量不会被提升。2，使用let或者var不能同时声明2个相同变量，如：var num, let num,会抛出错误
 2.const声明
 声明const变量的同时必须进行初始化，不然会抛出错误，如const name; //抛出错误 
-const声明同样是块级标识符
+const声明同样是块级标识符。在当前代码块内有效，同样也不会被提升至作用域顶部。常量不可以再次赋值。与其他语言中的常量不同
+的是，js中的常量如果是对象，则对象的值可以修改。例子
+3.临时死区
+用来描述let和const的不提升效果。例子
+4.循环中的块作用域绑定，非常适用
+5.循环中的函数 例子
+6.循环中的const声明，在普通的for循环中使用const，并修改它的值会抛出错误，但是在for-in和for-of循环中，使用const
+不会抛出错误，原因是：每次循环不会修改已有绑定，而是会创建一个新的绑定。
+7.全局块作用域绑定
+let\const和var的另一个区别是他们在全局作用域中的行为，var在全局作用域中声明变量，会为window添加一个属性，而其他俩不会，它只会
+遮蔽，不会覆盖。例子
+8.块级绑定最佳实践的进化
+默认使用const，只有在确切需要改变变量值的时候使用let，预料外的变量值的改变是很多bug的源头。
 */
 function getValue(condition){
     if(condition){
@@ -41,3 +53,51 @@ function getValue(condition){
     }
     //变量value此处不存在
 }
+//const对象
+const person = {
+    name: "nicholas"
+};
+person.name = "wfz";
+//抛出语法错误！
+person = {
+    name: "greg"
+}
+
+// 临时死区
+if(condition){
+    console.log(typeof value); //引用错误
+    let value = "blue";
+}
+
+console.log(typeof value); //undefined
+if(condition){
+    let value = "blue";
+}
+
+// 循环中的函数,强制性给i创建了一个副本，并存储为变量value
+var funcs = [];
+for(var i = 0; i < 10; i++){
+    funcs.push((function(value){
+        return function(){
+            console.log(value);  //输出0、1、2、3、4.。。。9
+        }
+    })(i))
+}
+
+var funcs = [];
+for(let i = 0; i < 10; i++){
+    funcs.push(function(){
+        console.log(i);
+    })
+}
+funcs.forEach(function(func){
+    func(); //输出0、1、。。9
+})
+
+// 全局块作用域绑定
+var RegExp = "hello";
+console.log(window.RegExp); //"hello"
+
+let RegExp = "hello";
+console.log(RegExp === window.RegExp); //false
+
